7d3b99aa9289215d555aa31bd208dcfcbb9c5983
vladtarnovskiy <vladtarnovski989@gmail.com>
2022-08-25 23:34:07 +0300

    feat: tracker

--- data/mythicCards/index.js
+++ data/mythicCards/index.js
@@ -1,9 +1,167 @@
-import brownCards from './brown';
-import blueCards from './blue';
-import greenCards from './green';
+import cardsDataBrown from './brown/index.js';
+import cardsDataBlue from './blue/index.js';
+import cardsDataGreen from './green/index.js';
+import ancientsData from '../ancients.js';
+import difficulties from '../difficulties.js';
 
-export {
-  brownCards,
-  blueCards,
-  greenCards
+const ancientsContainer = document.querySelector('.ancients-container')
+const ansientItems = ancientsContainer.querySelectorAll('.ancient-item')
+
+const stageFirstGreen = document.querySelector('.stage-first > .stage-set > .green')
+const stageFirstBrown = document.querySelector('.stage-first > .stage-set > .brown')
+const stageFirstblue = document.querySelector('.stage-first > .stage-set > .blue')
+
+const stageSecondGreen = document.querySelector('.stage-second > .stage-set > .green')
+const stageSecondBrown = document.querySelector('.stage-second > .stage-set > .brown')
+const stageSecondblue = document.querySelector('.stage-second > .stage-set > .blue')
+
+const stageThirdGreen = document.querySelector('.stage-third > .stage-set > .green')
+const stageThirdBrown = document.querySelector('.stage-third > .stage-set > .brown')
+const stageThirdblue = document.querySelector('.stage-third > .stage-set > .blue')
+
+let numberAncient
+let green = cardsDataGreen.slice()
+let brown = cardsDataBrown.slice()
+let blue = cardsDataBlue.slice()
+let board
+
+function getRandomNum (x) {
+  return (Math.round(Math.random() * x))
 }
+
+function getDeck () {
+
+  let setStageOne = []
+  let setStageTwo = []
+  let setStageThree = []
+
+  let countstageFirstGreen = ancientsData[numberAncient].firstStage.greenCards 
+  let countstageFirstBrown = ancientsData[numberAncient].firstStage.brownCards
+  let countstageFirstblue = ancientsData[numberAncient].firstStage.blueCards
+  const stageFirst = [countstageFirstGreen,countstageFirstBrown, countstageFirstblue]
+
+  for (let i=0; i < countstageFirstGreen; i++){
+    setStageOne.push(green.splice(getRandomNum((green.length-1)),1))
+  }
+  for (let i=0; i < countstageFirstBrown; i++){
+    setStageOne.push(brown.splice(getRandomNum((brown.length-1)),1))
+  }
+  for (let i=0; i < countstageFirstblue; i++){
+    setStageOne.push(blue.splice(getRandomNum((blue.length-1)),1))
+  }
+
+  let countstageSecondGreen = ancientsData[numberAncient].secondStage.greenCards 
+  let countstageSecondBrown = ancientsData[numberAncient].secondStage.brownCards
+  let countstageSecondblue = ancientsData[numberAncient].secondStage.blueCards
+  const stageSecond = [countstageSecondGreen,countstageSecondBrown, countstageSecondblue]
+
+  for (let i=0; i < countstageSecondGreen; i++){
+    setStageTwo.push(green.splice(getRandomNum((green.length-1)),1))
+  }
+  for (let i=0; i < countstageSecondBrown; i++){
+    setStageTwo.push(brown.splice(getRandomNum((brown.length-1)),1))
+  }
+  for (let i=0; i < countstageSecondblue; i++){
+    setStageTwo.push(blue.splice(getRandomNum((blue.length-1)),1))
+  }
+
+  let countstageThirdGreen = ancientsData[numberAncient].thirdStage.greenCards 
+  let countstageThirdBrown = ancientsData[numberAncient].thirdStage.brownCards
+  let countstageThirdblue = ancientsData[numberAncient].thirdStage.blueCards
+  const stageThird = [countstageThirdGreen,countstageThirdBrown, countstageThirdblue]
+
+  for (let i=0; i < countstageThirdGreen; i++){
+    setStageThree.push(green.splice(getRandomNum((green.length-1)),1))
+  }
+  for (let i=0; i < countstageThirdBrown; i++){
+    setStageThree.push(brown.splice(getRandomNum((brown.length-1)),1))
+  }
+  for (let i=0; i < countstageThirdblue; i++){
+    setStageThree.push(blue.splice(getRandomNum((blue.length-1)),1))
+  }
+  
+  let setCommon = setStageOne.concat(setStageTwo, setStageThree)
+
+  const deck = document.querySelector('.deck')
+  const card = document.querySelector('.card')
+
+  deck.addEventListener('click', () => {
+    card.classList.add('active')
+    let x
+    if (setStageOne.length>0){
+      x = setStageOne.splice(getRandomNum((setStageOne.length-1)),1)
+      if (x[0][0].color == 'green'){
+        stageFirstGreen.textContent = stageFirstGreen.textContent -1
+      } else if (x[0][0].color == 'brown'){
+        stageFirstBrown.textContent = stageFirstBrown.textContent - 1
+      } else if (x[0][0].color == 'blue'){
+        stageFirstblue.textContent = stageFirstblue.textContent - 1
+      }
+      
+    } else if (setStageTwo.length>0){
+      x = setStageTwo.splice(getRandomNum((setStageTwo.length-1)),1)
+      if (x[0][0].color == 'green'){
+        stageSecondGreen.textContent = stageSecondGreen.textContent -1
+      } else if (x[0][0].color == 'brown'){
+        stageSecondBrown.textContent = stageSecondBrown.textContent - 1
+      } else if (x[0][0].color == 'blue'){
+        stageSecondblue.textContent = stageSecondblue.textContent - 1
+      }
+
+    } else if (setStageThree.length>0){
+      x = setStageThree.splice(getRandomNum((setStageThree.length-1)),1)
+      if (x[0][0].color == 'green'){
+        stageThirdGreen.textContent = stageThirdGreen.textContent -1
+      } else if (x[0][0].color == 'brown'){
+        stageThirdBrown.textContent = stageThirdBrown.textContent - 1
+      } else if (x[0][0].color == 'blue'){
+        stageThirdblue.textContent = stageThirdblue.textContent - 1
+      }
+    } 
+    else {
+      card.classList.remove('active')
+      alert('Deck Ended! Choose new ancient!!')
+    }
+    console.log(x[0][0].cardFace)
+    card.style.backgroundImage = `url(${x[0][0].cardFace})`
+  })
+
+  console.log(setStageOne)
+  console.log(setStageTwo)
+  console.log(setStageThree)
+  console.log(green)
+  console.log(setCommon)
+}
+
+function displayTraker () {
+  ansientItems.forEach((item,index) => {
+    item.addEventListener('click', (e) => {
+
+      board = [
+        [ancientsData[index].firstStage.greenCards, ancientsData[index].firstStage.brownCards, ancientsData[index].firstStage.blueCards],
+        [ancientsData[index].secondStage.greenCards, ancientsData[index].secondStage.brownCards, ancientsData[index].secondStage.blueCards],
+        [ancientsData[index].thirdStage.greenCards, ancientsData[index].thirdStage.brownCards, ancientsData[index].thirdStage.blueCards],
+      ]
+
+      stageFirstGreen.textContent = board[0][0]
+      stageFirstBrown.textContent = board[0][1]
+      stageFirstblue.textContent = board[0][2]
+
+      stageSecondGreen.textContent = board[1][0]
+      stageSecondBrown.textContent = board[1][1] 
+      stageSecondblue.textContent = board[1][2]
+
+      stageThirdGreen.textContent = board[2][0]
+      stageThirdBrown.textContent = board[2][1]
+      stageThirdblue.textContent = board[2][2]
+
+      numberAncient = index
+      getDeck ()
+    })
+  })
+}
+
+displayTraker()
+
+
+
