066c2fc59b30ee64a53bcfca090ab1fabefd6abf
vladtarnovskiy <vladtarnovski989@gmail.com>
2022-09-01 22:23:53 +0300

    fix: hide ended deck

--- data/mythicCards/index.js
+++ data/mythicCards/index.js
@@ -24,21 +24,18 @@ const stageOneTitle = document.querySelector('.stage-first > .stage-title')
 const stageTwoTitle = document.querySelector('.stage-second > .stage-title')
 const stageThirdTitle = document.querySelector('.stage-third > .stage-title')
 
+const levelButton = document.querySelectorAll('.level-item')
+const difficultyContainer = document.querySelector('.dificulty-container-inner')
+const deckContainer = document.querySelector('.deck-container-inner')
+
 const deck = document.querySelector('.deck')
 const card = document.querySelector('.card')
 
 let numberAncient
-let green
-let brown
-let blue
+let green = cardsDataGreen.slice()
+let brown = cardsDataBrown.slice()
+let blue = cardsDataBlue.slice()
 let board
-
-let flagAncient = [
-  { ansient: 'false' },
-  { ansient: 'false' },
-  { ansient: 'false' },
-  { ansient: 'false' }
-]
 
 function getRandomNum(x) {
   return (Math.round(Math.random() * x))
@@ -52,7 +49,6 @@ function getDeck() {
   let setStageOne = []
   let setStageTwo = []
   let setStageThree = []
-
   for (let i = 0; i < countstageFirstGreen; i++) {
     setStageOne.push(green.splice(getRandomNum((green.length - 1)), 1))
   }
@@ -66,7 +62,6 @@ function getDeck() {
   let countstageSecondGreen = ancientsData[numberAncient].secondStage.greenCards
   let countstageSecondBrown = ancientsData[numberAncient].secondStage.brownCards
   let countstageSecondblue = ancientsData[numberAncient].secondStage.blueCards
-
   for (let i = 0; i < countstageSecondGreen; i++) {
     setStageTwo.push(green.splice(getRandomNum((green.length - 1)), 1))
   }
@@ -80,7 +75,6 @@ function getDeck() {
   let countstageThirdGreen = ancientsData[numberAncient].thirdStage.greenCards
   let countstageThirdBrown = ancientsData[numberAncient].thirdStage.brownCards
   let countstageThirdblue = ancientsData[numberAncient].thirdStage.blueCards
-
   for (let i = 0; i < countstageThirdGreen; i++) {
     setStageThree.push(green.splice(getRandomNum((green.length - 1)), 1))
   }
@@ -94,7 +88,6 @@ function getDeck() {
   function click() {
     card.classList.add('active')
     let x
-
     if (setStageOne.length > 0) {
       x = setStageOne.splice(getRandomNum((setStageOne.length - 1)), 1)
       card.style.backgroundImage = `url(${x[0][0].cardFace})`
@@ -106,7 +99,6 @@ function getDeck() {
       } else if (x[0][0].color == 'blue') {
         stageFirstblue.textContent = stageFirstblue.textContent - 1
       }
-
     } else if (setStageTwo.length > 0) {
       x = setStageTwo.splice(getRandomNum((setStageTwo.length - 1)), 1)
       card.style.backgroundImage = `url(${x[0][0].cardFace})`
@@ -118,7 +110,6 @@ function getDeck() {
       } else if (x[0][0].color == 'blue') {
         stageSecondblue.textContent = stageSecondblue.textContent - 1
       }
-
     } else if (setStageThree.length > 0) {
       x = setStageThree.splice(getRandomNum((setStageThree.length - 1)), 1)
       card.style.backgroundImage = `url(${x[0][0].cardFace})`
@@ -130,12 +121,12 @@ function getDeck() {
       } else if (x[0][0].color == 'blue') {
         stageThirdblue.textContent = stageThirdblue.textContent - 1
       }
-
     } else {
       card.classList.remove('active')
+      deckContainer.classList.remove('visible')
+      difficultyContainer.classList.remove('visible')
       alert('Card ended! Pleace, choose new ancient!!')
     }
-
     ancientsContainer.addEventListener('click', ()=>{
       deck.removeEventListener('click', click, false)
       stageOneTitle.classList.remove('stage-active')
@@ -143,14 +134,28 @@ function getDeck() {
       stageThirdTitle.classList.remove('stage-active')
     })
   }
-
   deck.addEventListener('click', click, false)
 }
 
 function displayTraker() {
+  levelButton.forEach((item, index) => {
+    item.addEventListener('click', (e) => {
+
+      levelButton.forEach((item, index) => {
+        item.classList.remove('level-item-active')
+      })
+
+      if (e.target) {
+        e.target.classList.add('level-item-active')
+      }
+
+    })
+  })
+
   ansientItems.forEach((item, index) => {
 
     function fuck() {
+      difficultyContainer.classList.add('visible')
       card.classList.remove('active')
 
       ansientItems.forEach((item, index) => {
@@ -158,10 +163,6 @@ function displayTraker() {
       })
 
       item.classList.add('ancient-item-active')
-
-      green = cardsDataGreen.slice()
-      brown = cardsDataBrown.slice()
-      blue = cardsDataBlue.slice()
 
       board = [
         [ancientsData[index].firstStage.greenCards, ancientsData[index].firstStage.brownCards, ancientsData[index].firstStage.blueCards],
@@ -182,35 +183,47 @@ function displayTraker() {
       stageThirdblue.textContent = board[2][2]
 
       numberAncient = index
-    
-      getDeck()
+      levelButton.forEach((item) => {
+        item.addEventListener('click', changeDificulties)
+      })
     }
-    
     item.addEventListener('click', fuck, false)
   })
 }
 
-displayTraker()
 
-function changeDificulties() {
-  const levelButton = document.querySelectorAll('.level-item')
+function changeDificulties(e) {
+    deckContainer.classList.add('visible')
+    green = cardsDataGreen.slice()
+    brown = cardsDataBrown.slice()
+    blue = cardsDataBlue.slice()
 
-  levelButton.forEach((item, index) => {
-    item.addEventListener('click', (e) => {
-
-      levelButton.forEach((item, index) => {
-        item.classList.remove('level-item-active')
-      })
-
-      if (e.target) {
-        e.target.classList.add('level-item-active')
-      }
-
-    })
-  })
+  if (e.target.id == 'middle'){
+    green = cardsDataGreen.slice()
+    brown = cardsDataBrown.slice()
+    blue = cardsDataBlue.slice()
+    console.log(green)
+    console.log(e.target.id)
+  } else if (e.target.id == 'easy'){
+    green = green.filter(function(a) {return a.difficulty !== 'hard'})
+    brown = brown.filter(function(a) {return a.difficulty !== 'hard'})
+    blue = blue.filter(function(a) {return a.difficulty !== 'hard'})
+    console.log(green)
+    console.log(e.target.id)
+  } else if (e.target.id == 'hard'){
+    green = green.filter(function(a) {return a.difficulty !== 'easy'})
+    brown = brown.filter(function(a) {return a.difficulty !== 'easy'})
+    blue = blue.filter(function(a) {return a.difficulty !== 'easy'})
+    console.log(green)
+    console.log(e.target.id)
+  }
+  displayTraker()
+  getDeck()
 }
 
-changeDificulties()
+
+displayTraker()
+
 
 
 
